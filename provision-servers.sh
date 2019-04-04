c2e5094b3a271b954532b6caa834bfc0af9b9d49
cixstian <cixstian@cixstians-MacBook-Pro.local>
2019-04-04 12:56:44 +0200

    Update to better standardisation and copy info to localhost

--- provision-servers.sh
+++ provision-servers.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+# ************************ CONFIG VARS ************************
+
 NODES=2
+
+# NETWORK='FLANNEL'
+NETWORK='CALICO'
+# NETWORK='CANAL' # uses FLANNEL overlay with CALICO Network Policies
+
+# K8_VERSION="1.13.5"
+# CRIO_VERSION="1.13"
+
+K8_VERSION="1.14.0"
+CRIO_VERSION="1.14"
 
 # Use "doctl compute ssh-key list" to get this
 # DO_KEYS="23696360"
 DO_KEYS="24225182,24202611"
 
-rm provision-nodes.log
+
+# ************************ SCRIPT ************************
+
+rm provision-servers.log
 rm ansible/ansible.conf
 
 NODE_STRING="" && for (( i = 1; i <= $NODES; i++ )); do NODE_STRING="$NODE_STRING node$i"; done
@@ -20,10 +35,10 @@ doctl compute droplet create master $NODE_STRING \
   --size s-2vcpu-2gb  \
   --format ID,Name,PublicIPv4,PrivateIPv4,Status \
   --enable-private-networking \
-  --wait >> provision-nodes.log
+  --wait >> provision-servers.log
 
-MASTER_PUBLIC_IP=`cat provision-nodes.log | grep master | awk '{print $3}'`
-MASTER_PRIVATE_IP=`cat provision-nodes.log | grep master | awk '{print $4}'`
+MASTER_PUBLIC_IP=`cat provision-servers.log | grep master | awk '{print $3}'`
+MASTER_PRIVATE_IP=`cat provision-servers.log | grep master | awk '{print $4}'`
 
 cat <<EOT >> ansible/ansible.conf
 [masters]
@@ -33,22 +48,32 @@ EOT
 echo "" >> ansible/ansible.conf
 echo "[workers]" >> ansible/ansible.conf
 for (( i = 1; i <= $NODES; i++ )); do
-  NODE_IP=`cat provision-nodes.log | grep "node$i" | awk '{print $3}'`
+  NODE_IP=`cat provision-servers.log | grep "node$i" | awk '{print $3}'`
   echo "worker$i ansible_host=$NODE_IP ansible_user=root" >> ansible/ansible.conf
 done
 echo "" >> ansible/ansible.conf
+
+if [ "$NETWORK" == "CALICO" ]; then CIDR="192.168.0.0/16";
+elif [ "$NETWORK" == "FLANNEL" ] || [ "$NETWORK" == "CANAL" ]; then CIDR="10.244.0.0/16";
+fi
 
 cat <<EOT >> ansible/ansible.conf
 [all:vars]
 ansible_python_interpreter=/usr/bin/python3
 master_public_ip=$MASTER_PUBLIC_IP
 master_private_ip=$MASTER_PRIVATE_IP
-k8_version=1.13.5
-crio_version=1.13
-cidr=192.168.0.0/16
-network=Calico
+k8_version=$K8_VERSION
+crio_version=$CRIO_VERSION
+cidr=$CIDR
+network=$NETWORK
 EOT
 
-echo "\nSSH command to master is: ssh root@$MASTER_PUBLIC_IP \n"
-
 sleep 10
+
+echo "
+SSH command to master is:       ssh root@$MASTER_PUBLIC_IP"
+for (( i = 1; i <= $NODES; i++ )); do
+  NODE_IP=`cat provision-servers.log | grep "node$i" | awk '{print $3}'`
+  echo "SSH command to node$i is:         ssh root@$NODE_IP"
+done
+echo ""
