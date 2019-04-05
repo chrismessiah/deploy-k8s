684cd50660fb82b5db37467cf901a6d452fb0953
cixstian <cixstian@cixstians-MacBook-Pro.local>
2019-04-05 16:10:15 +0200

    fix script to use calico

--- istio/run.sh
+++ istio/run.sh
@@ -1,20 +1,21 @@
 
+# download istio
 curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.1.1 sh -
-
 
 ################################################################################
 # See https://istio.io/docs/setup/kubernetes/install/helm/ for more info on the
 # installation procedure
 ################################################################################
 
-
 kubectl create namespace istio-system
 
 # Use helm client to install Istio CRDs
+cd istio
+cd istio-1.1.1
 helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f -
 
 # Verify installation, should return 53 or 58
-kubectl get crds | grep 'istio.io\|certmanager.k8s.io' | wc -l
+watch -n 2 "kubectl get crds | grep 'istio.io\|certmanager.k8s.io' | wc -l"
 
 # choose a configuration profile described in
 # https://istio.io/docs/setup/kubernetes/install/helm/
@@ -26,3 +27,5 @@ helm template install/kubernetes/helm/istio --name istio --namespace istio-syste
 # Demo profile
 helm template install/kubernetes/helm/istio --name istio --namespace istio-system \
     --values install/kubernetes/helm/istio/values-istio-demo.yaml | kubectl apply -f -
+
+watch -n 2 "kubectl get pods --all-namespaces"
