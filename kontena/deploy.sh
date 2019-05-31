da26a0425d61ed7a93e440a7fef076458f9d7b14
cixstian <cixstian@cixstians-MacBook-Pro.local>
2019-05-31 13:05:57 +0200

    Update deploy.sh

--- kontena/deploy.sh
+++ kontena/deploy.sh
@@ -1,4 +1,3 @@
-chpharos auto
 
 # DO_SSH_KEY="24225182" # private
 DO_SSH_KEY="24202611"
@@ -9,6 +8,8 @@ doctl compute droplet create do-host01 \
   --size s-1vcpu-2gb  \
   --wait
 
+PUBLIC_IP_HOST_1=$(doctl compute droplet get $(doctl compute droplet list | grep "do-host01" | cut -d' ' -f1) --format PublicIPv4 --no-header)
+
 # HC_SSH_KEY="christian" # private
 HC_SSH_KEY="821926"
 hcloud server create \
@@ -18,8 +19,16 @@ hcloud server create \
   --image ubuntu-18.04 \
   --ssh-key $HC_SSH_KEY
 
-# ***** update cluster.yml with IPv4 ******
+PUBLIC_IP_HOST_2=$(hcloud server list -o columns=ipv4 -o noheader)
 
+echo "
+  Please update cluster.yml with the following IPv4
+
+  Digital Ocean Host 1:     $PUBLIC_IP_HOST_1
+  Hetzner Cloud Host 1:     $PUBLIC_IP_HOST_2
+"
+
+chpharos auto
 pharos up
 
 pharos kubeconfig  -n pharos-cluster > ~/.kube/config
