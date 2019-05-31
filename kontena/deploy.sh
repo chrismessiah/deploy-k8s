5449127115c2fb9b3b9b6282ef9c8b58f3721efe
cixstian <cixstian@cixstians-MacBook-Pro.local>
2019-05-31 10:48:50 +0200

    Update deploy.sh

--- kontena/deploy.sh
+++ kontena/deploy.sh
@@ -1,20 +1,22 @@
-cd provision-kontena-cluster
-
 chpharos auto
 
+# DO_SSH_KEY="24225182" # private
+DO_SSH_KEY="24202611"
 doctl compute droplet create do-host01 \
-  --ssh-keys 24225182 \
+  --ssh-keys $DO_SSH_KEY \
   --region fra1 \
   --image ubuntu-18-04-x64 \
   --size s-1vcpu-2gb  \
   --wait
 
+# HC_SSH_KEY="christian" # private
+HC_SSH_KEY="821926"
 hcloud server create \
   --name hc-host01 \
   --type cx11 \
   --location nbg1 \
   --image ubuntu-18.04 \
-  --ssh-key christian
+  --ssh-key $HC_SSH_KEY
 
 # ***** update cluster.yml with IPv4 ******
 
